4a92f8178c1b055f529ed45254ae696ed66856fd
Artem Kostenko <artem.kostenko@pokupon.ua>
2013-03-19 04:39:41 +0200

    ajax;

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,3 +1,5 @@
 class Domain < ActiveRecord::Base
+validates_presence_of  :sphinx_config_path ,:if => lambda{|f| FileTest.exists?(f.sphinx_config_path) }
+validates_presence_of  :domain_name
   attr_accessible :domain_name, :sphinx_config_file_indexing_period, :sphinx_config_file_offset, :sphinx_config_path, :weight
 end
