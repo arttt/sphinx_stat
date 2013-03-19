52a45a0b98debf6bb310ab6de38b810277ab7d9b
Artem Kostenko <artem.kostenko@pokupon.ua>
2013-03-19 03:55:40 +0200

    ajax;

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,7 +7,8 @@ class MainController < ApplicationController
 		d1 = Date.new(2013,03,19)
 		d2 = Date.new(2013,03,20)
 		parser = SphinxCfgParser.new
-		@parse_result = parser.process(d1,d2,"")[0..100]
+		log_files = ["/home/a/ruby_test/query.log"]
+		@parse_result = parser.process(d1,d2,log_files)[0..100]
 		
 		render 'external/sphinx_data'
 	end
