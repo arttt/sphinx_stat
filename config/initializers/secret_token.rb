2d3673a8954ab2dd8b4b103912a15d2de30ebb63
artem-kostenko <artem.kostenko@pokupon.ua>
2013-03-21 12:15:11 +0200

    new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sphix::Application.config.secret_token = '9897273d40835e809085131fbf27b68a6cd5b5873847be2b66512a3c7824ba09b0d8b7c2e7ae2999dd8ea976681cf9716a0316ad3adf95bf36781085c7ffb7c4'
